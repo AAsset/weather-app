806ac52b1ed807dd4d24c9279f0c39a173039d62
Asset <asset.aktay@gmail.com>
2021-03-09 00:38:58 +0600

    add search

---
+++ src/app/ui/search/search.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, ChangeDetectionStrategy, Output, EventEmitter } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SearchComponent {
+  @Input() placeholder: string = '';
+  @Output() searchCriteria = new EventEmitter();
+  searchword = new FormControl('', [Validators.required]);
+
+  onSearch() {
+    if (this.searchword.invalid) {
+      this.searchword.markAsDirty();
+      return;
+    }
+    this.searchCriteria.emit(this.searchword.value.trim());
+  }
+}
