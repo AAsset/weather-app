f75c866cf358d9cf1dbc5631d85cdd606732dba4
Asset <asset.aktay@gmail.com>
2021-03-09 00:18:57 +0600

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { RoutingModule } from './routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
