42174c38ff882cd629b1016ae0b2a185c12b8521
Asset <asset.aktay@gmail.com>
2021-04-25 17:09:19 +0600

    fix prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  openweather_url: 'http://api.openweathermap.org/data/2.5/forecast',
+  openweather_url: 'http://api.openweathermap.org/data/2.5/',
   openweather_api_key: "2adfe9a8080c1272b46cf306a8c6de6f"
 };
