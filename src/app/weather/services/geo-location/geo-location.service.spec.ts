03063bfef41365c6868cc5a25fd0573fa1d51e75
Asset <asset.aktay@gmail.com>
2021-05-08 21:16:18 +0600

    test geo location service

--- src/app/weather/services/geo-location/geo-location.service.spec.ts
+++ src/app/weather/services/geo-location/geo-location.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { GeoLocationService } from './geo-location.service';
 
@@ -6,11 +7,34 @@ describe('GeoLocationService', () => {
   let service: GeoLocationService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [GeoLocationService]
+    });
     service = TestBed.inject(GeoLocationService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it("getPosition() should return position", () => {
+    const mockPosition = {
+      coords: {
+        latitude: 43.25,
+        longitude: 76.95
+      },
+      timestamp: 1620453600
+    };
+    const fake =  {
+      getPosition: () => of(mockPosition)
+    };
+    service = fake as GeoLocationService;
+
+    service.getPosition().subscribe(data => {
+      expect(data).toBeDefined();
+      expect(data).not.toBe(null);
+      expect(data.coords.latitude).toEqual(43.25);
+      expect(data.coords.longitude).toEqual(76.95);
+    })
   });
 });
